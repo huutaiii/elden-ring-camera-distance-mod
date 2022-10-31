bb1764eede459f6906df56fad86481d3aadd5e23
huutai <truongtai137@gmail.com>
2022-10-31 21:38:04 +0700

    cleaning up stuff

--- EldenRingCameraDistance/dllmain.cpp
+++ EldenRingCameraDistance/dllmain.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <xmmintrin.h>
 
-//constexpr unsigned int ASM_PROC_SIZE = 5;
 constexpr unsigned int JMP_SIZE = 14;
 
 extern "C"
@@ -18,25 +17,23 @@ extern "C"
 
 DWORD WINAPI MainThread(LPVOID lpParam)
 {
-    //std::vector<uint16_t> original({ 0xF3, 0x0F, 0x11, 0xBB, 0xB8, 0x01, 0x00, 0x00 });
-    //std::vector<uint8_t> replacement({ 0xE8, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90});
-    std::vector<uint16_t> original({ 0xF3, 0x0F, 0x11, 0xBB, 0xB8, 0x01 });
+    std::vector<uint16_t> original({ 0xF3, 0x0F, 0x11, 0xBB, 0xB8, 0x01 }); // movss [rbx + 1B8], xmm7 (2 MSBytes truncated)
     std::vector<uint8_t> replacement({ 0xFF, 0x25, 0x00, 0x00, 0x00, 0x00 }); // ff 25 00 00 00 00
     uintptr_t hookAddress = ModUtils::SigScan(original);
     if (hookAddress)
     {
         uintptr_t targetAddress = (uintptr_t)&CameraDistance;
+
+        ModUtils::Log("Applying patch at %p", hookAddress);
         ModUtils::Log("Target address: %p", targetAddress);
-    //    uintptr_t relativeAddress = (uintptr_t)&CameraDistance - (hookAddress + 5);
-    //    replacement[1] = relativeAddress & 0xFF;
-    //    replacement[2] = (relativeAddress & 0xFF00) >> 8;
-    //    replacement[3] = (relativeAddress & 0xFF0000) >> 16;
-    //    replacement[4] = (relativeAddress & 0xFF000000) >> 24;
-    //    ReturnAddress = hookAddress - ((uintptr_t)&CameraDistance + ASM_PROC_SIZE);
+
         ModUtils::Replace(hookAddress, original, replacement);
-        //ModUtils::MemCopy(hookAddress, (uintptr_t)0x0000000025ff, 6);
         ModUtils::MemCopy(hookAddress + (JMP_SIZE - 8), (uintptr_t)&targetAddress, 8);
         ReturnAddress = hookAddress + JMP_SIZE;
+    }
+    else
+    {
+        ModUtils::RaiseError(ModUtils::GetModuleName() + ": Search failed. Nothing is modified.");
     }
     ModUtils::CloseLog();
     return 0;
